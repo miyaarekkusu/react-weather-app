6e2585a3d07e11217c091d28385e56adf742f146
alex.dev <alexjmm456@gmail.com>
2026-04-07 17:20:52 +0900

    OpenWeatherAPIキー設定とフェッチ確認

---
+++ weather-app/src/api.ts
@@ -0,0 +1,11 @@
+const API_KEY = import.meta.env.VITE_API_KEY
+
+
+export async function getWeather({lat, lon}: {lat: number, lon: number}) {
+     const res = await fetch(
+        `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&units=metric&exclude=minutely,alerts&appid=${API_KEY}`
+    )
+    
+    const data = await res.json()
+    return data
+}
